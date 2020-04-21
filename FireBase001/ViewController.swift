d124b6c9604d72b09d72958e4b3540e4ec42486e
HoangNM <hoangnm@ttc-solutions.com>
2020-04-21 14:51:48 +0700

    Initial Commit

---
+++ FireBase001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireBase001
+//
+//  Created by Hoang on 4/21/20.
+//  Copyright © 2020 Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
