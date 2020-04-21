463e5a60aff9a074bc67a6f0b37e982243718726
HoangNM <hoangnm@ttc-solutions.com>
2020-04-21 15:54:51 +0700

    -My First Project

--- FireBase001/ViewController.swift
+++ FireBase001/ViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Connect to FirebaseDatabase
+        let firRef = Database.database().reference(fromURL: "https://fir-001app.firebaseio.com/")
+//        firRef.child("FirstData")
+//        firRef.child("FirstData").setValue(["MyFirstValue":"HelloWorld!"])
+        
+        
+        Auth.auth().createUser(withEmail: "myfirstemail@gmail.com", password: "abc123") { (result, err) in
+            if let errCode = err {
+                print("HelloSenpai: Error here\(errCode)")
+            } else {
+                print("HelloSenpai: Success here\(result?.description)")
+            }
+        }
+        
+        
         // Do any additional setup after loading the view.
     }
 
